c37d01579c8250d1ac3af0fc6727e2d1fc4dc8e0
Rabeea Safadi <rabeeasafadi@gmail.com>
2022-11-29 14:25:33 +0200

    book preview page styled

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,16 +1,19 @@
 import { useLocation } from 'react-router-dom';
 import { IBookProps } from '../components/Book/Book';
+import styles from './BookPage.module.css';
 
 const BookPage = () => {
   const book = useLocation().state as IBookProps;
 
   return (
-    <div>
-      <div>{book.title}</div>
-      <div>{book.author}</div>
-      <div>{book.uuid}</div>
-      <div>{book.price}</div>
-      <img src={book.coverURL} alt="Book cover" />
+    <div className={styles.book}>
+      <div className={styles['book-info']}>
+        <div className={styles['book-title']}>{book.title}</div>
+        <div className={styles['book-author']}>{book.author}</div>
+        <div className={styles['book-uuid']}>{book.uuid}</div>
+        <div className={styles['book-price']}>{book.price}</div>
+      </div>
+      <img className={styles['book-image']} src={book.coverURL} alt="Book cover" />
     </div>
   );
 };

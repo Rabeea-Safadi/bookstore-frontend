3953d4f358cf6e8b216324a6583a87c276ed8b56
Rabeea Safadi <rabeeasafadi@gmail.com>
2022-11-29 14:49:16 +0200

    add book page added + styled

--- src/pages/AddPage/AddPage.tsx
+++ src/pages/AddPage/AddPage.tsx
@@ -1,5 +1,15 @@
+import styles from './AddPage.module.css';
+
 const AddPage = () => {
-  return <div>Add Page</div>;
+  return (
+    <form className={styles['book-form']}>
+      <input className={styles.book} type="text" placeholder="Book Title" minLength={3} maxLength={25} required />
+      <input className={styles.book} type="text" placeholder="Book Author" minLength={3} maxLength={25} required />
+      <input className={styles.book} type="number" placeholder="Book Price" min={1} max={1000} required />
+      <input className={styles.book} type="url" placeholder="Book Cover URL" required />
+      <input className={styles.book} type="submit" value="Add Book" />
+    </form>
+  );
 };
 
 export default AddPage;

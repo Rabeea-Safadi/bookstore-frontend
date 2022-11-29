ec13ced769d6e0c5f17c20104b03ca2e1c6b6860
Rabeea Safadi <rabeeasafadi@gmail.com>
2022-11-29 16:11:21 +0200

    add book now works on dummy data

--- src/pages/AddPage/AddPage.tsx
+++ src/pages/AddPage/AddPage.tsx
@@ -1,12 +1,48 @@
+import { FC, FormEvent, useRef } from 'react';
+import { v4 } from 'uuid';
+import { IBookProps } from '../../components/Book/Book';
 import styles from './AddPage.module.css';
 
-const AddPage = () => {
+export interface IAddPageProps {
+  addBookHandler: (book: IBookProps) => void;
+}
+
+const AddPage: FC<IAddPageProps> = ({ addBookHandler }) => {
+  const titleRef = useRef<HTMLInputElement>();
+  const authorRef = useRef<HTMLInputElement>();
+  const priceRef = useRef<HTMLInputElement>();
+  const urlRef = useRef<HTMLInputElement>();
+
+  function submitHandler(event: FormEvent) {
+    event.preventDefault();
+
+    const book: IBookProps = {
+      author: authorRef.current?.value!,
+      title: titleRef.current?.value!,
+      price: Number(priceRef.current?.value!),
+      coverURL: urlRef.current?.value!,
+      uuid: v4(),
+    };
+
+    addBookHandler(book);
+
+    alert('added book');
+  }
+
   return (
-    <form className={styles['book-form']}>
-      <input className={styles.book} type="text" placeholder="Book Title" minLength={3} maxLength={25} required />
-      <input className={styles.book} type="text" placeholder="Book Author" minLength={3} maxLength={25} required />
-      <input className={styles.book} type="number" placeholder="Book Price" min={1} max={1000} required />
-      <input className={styles.book} type="url" placeholder="Book Cover URL" required />
+    <form className={styles['book-form']} onSubmit={submitHandler}>
+      <input ref={titleRef} className={styles.book} type="text" placeholder="Book Title" minLength={3} maxLength={25} required />
+      <input
+        ref={authorRef}
+        className={styles.book}
+        type="text"
+        placeholder="Book Author"
+        minLength={3}
+        maxLength={25}
+        required
+      />
+      <input ref={priceRef} className={styles.book} type="number" placeholder="Book Price" min={1} max={1000} required />
+      <input ref={urlRef} className={styles.book} type="url" placeholder="Book Cover URL" required />
       <input className={styles.book} type="submit" value="Add Book" />
     </form>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,34 +1,13 @@
+import { FC } from 'react';
 import { IBookProps } from '../../components/Book/Book';
 import BookList from '../../components/BookList/BookList';
 
-const FAKE_BOOKS: IBookProps[] = [
-  {
-    uuid: '321321321',
-    author: 'j.k rowling',
-    title: 'harry potter',
-    price: 25,
-    coverURL: 'https://media.harrypotterfanzone.com/deathly-hallows-us-childrens-edition.jpg',
-  },
-  {
-    uuid: '4143',
-    author: 'j.k rowling',
-    title: 'harry potter 2',
-    price: 25,
-    coverURL:
-      'https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/7/30/1406719264487/fd90e162-93de-41b1-aea5-f8e49227e85b-1360x2040.jpeg?width=700&quality=85&auto=format&fit=max&s=28732c5a7caa1d1249d3f420e247479b',
-  },
-  {
-    uuid: '54121',
-    author: 'j.k rowling',
-    title: 'harry potter 3',
-    price: 25,
-    coverURL:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2016%2F09%2F9781408855652-png.jpg',
-  },
-];
+export interface IHomePageProps {
+  books: IBookProps[];
+}
 
-const HomePage = () => {
-  return <BookList books={FAKE_BOOKS} />;
+const HomePage: FC<IHomePageProps> = ({ books }) => {
+  return <BookList books={books} />;
 };
 
 export default HomePage;
